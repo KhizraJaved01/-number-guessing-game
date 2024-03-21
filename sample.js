15d8c5e2232db6b609f69bfad21fb99859187415
Khizra Javed <159693618+KhizraJaved01@users.noreply.github.com>
2024-03-20 18:12:22 -0700

    added all files in git

---
+++ sample.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+async function guessNumber() {
+    let continueGame = true;
+    while (continueGame) {
+        const systemGeneratedNo = Math.floor(Math.random() * 20);
+        const answers = await inquirer.prompt([
+            {
+                type: "number",
+                name: "userGuess",
+                message: "Enter any number b/w 1 to 20"
+            }
+        ]);
+        const { userGuess } = answers;
+        if (userGuess === systemGeneratedNo) {
+            console.log(`${userGuess} is the right answer!`);
+            console.log("Right guess you win!");
+        }
+        else {
+            console.log("Sorry, better luck next time!");
+            console.log(`Correct answer was ${systemGeneratedNo}.`);
+        }
+        const { playAgain } = await inquirer.prompt([
+            {
+                type: "confirm",
+                name: "playAgain",
+                message: "Do you want to play again",
+                default: false
+            }
+        ]);
+        continueGame = playAgain;
+    }
+}
+guessNumber();
